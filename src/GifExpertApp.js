29a93f365ec57ceea14ebb3417277d5675a50d78
Miguel de la Torre <madelatorre89@gmail.com>
2022-01-08 12:40:13 +0100

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = ( ) => {
+
+    const [ categories, setCategories ] = useState(['Walter White']);
+
+    // const handleAdd = ( ) => {
+    //     // setCategories( [...categories, 'Miguel'] );
+    //     setCategories( cats => [...categories, 'Miguel'] );
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr />
+
+            <ol>
+                {
+                    categories.map( category => (
+                        <GifGrid 
+                            key={ category }
+                            category={ category } 
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    );
+}
+
+export default GifExpertApp;
